c5a82380258ec82e5b59c341033e3b0bea0a847c
Bladimir Lopez <lopezhumbertbladimir425@gmail.com>
2023-09-12 21:46:22 -0600

    Modifife la vista de la factura y agregue el framework Express

--- src/components/recibos.jsx
+++ src/components/recibos.jsx
@@ -1,47 +1,46 @@
 const Recibos = () => {
-    return (
-    <div>
-        
-        <div id="wrapper">
-            <div id="content">
-                <div class="container">
-
-                    <div class="box G texto centro">Los recibos digitales generados para cada transacción estarán
-                        disponibles aquí. Los comerciantes podrán acceder a estos recibos y también enviarlos por correo
-                        electrónico o SMS a los clientes.</div>
-
-                    <div class="box G texto">
-                        <div class="titulo centro">FACTURAS</div>
-                        <table class="tftable">
-                            <thead>
-                                <th>ID</th>
-                                <th>FECHA</th>
-                                <th>MONTO</th>
-                                <th>CLIENTE</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>63</td>
-                                    <td>15/09/2023</td>
-                                    <td>160.90</td>
-                                    <td>Fulano</td>
-                                </tr>
-                                <tr>
-                                    <td>94</td>
-                                    <td>19/09/2023</td>
-                                    <td>65.32</td>
-                                    <td>Mengano</td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                    </div>
-
-                </div>
-            </div>
+  return (
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div class="max-w-4xl p-8 bg-white shadow-lg rounded-lg">
+        <div class="text-xl font-bold mb-4">
+          {" "}
+          Los recibos digitales generados para cada transacción estarán
+          disponibles aquí. Los comerciantes podrán acceder a estos recibos y
+          también enviarlos por correo electrónico o SMS a los clientes.
         </div>
 
+        <div class="flex items-center justify-between mb-2">
+          <div class="text-lg font-semibold">FACTURAS</div>
+          <button class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Nueva factura
+          </button>
+        </div>
+        <table class="w-full border-collapse">
+          <thead>
+            <tr class="bg-gray-200">
+              <th class="px-4 py-2 border">ID</th>
+              <th class="px-4 py-2 border">FECHA</th>
+              <th class="px-4 py-2 border">MONTO</th>
+              <th class="px-4 py-2 border">CLIENTE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="px-4 py-2 border">63</td>
+              <td class="px-4 py-2 border">15/09/2023</td>
+              <td class="px-4 py-2 border">160.90</td>
+              <td class="px-4 py-2 border">Fulano</td>
+            </tr>
+            <tr>
+              <td class="px-4 py-2 border">94</td>
+              <td class="px-4 py-2 border">19/09/2023</td>
+              <td class="px-4 py-2 border">65.32</td>
+              <td class="px-4 py-2 border">Mengano</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    )
+  );
 };
 export default Recibos;
