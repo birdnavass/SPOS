1df7c7ed9988b7c38cef1ecd47f35b282a5c94c2
arte988 <temito988@gmail.com>
2023-09-20 07:47:27 -0600

    Adelanto de graficos

--- src/components/control.jsx
+++ src/components/control.jsx
@@ -1,22 +1,62 @@
-const Control = () => {
-    return (
-    <div>
-        
-        <div id="wrapper">
-            <div id="content">
-                <div className="container">
+"use client";
 
-                    <div className="box G texto centro">Un resumen visual de estadísticas y métricas clave relacionadas con las
-                        ventas y el rendimiento general de la plataforma. Esto proporcionará a los comerciantes una visión
-                        rápida de cómo está funcionando su negocio.
-                    </div>
+import { BadgeDelta, Card, Grid, DeltaType, Flex, Metric, ProgressBar, Text } from "@tremor/react";
 
-
-                </div>
-            </div>
-        </div>
-
-    </div>
-    )
+type Kpi = {
+  title: string;
+  metric: string;
+  progress: number;
+  target: string;
+  delta: string;
+  deltaType: DeltaType;
 };
-export default Control;
+
+const kpiData: Kpi[] = [
+  {
+    title: "Sales",
+    metric: "$ 12,699",
+    progress: 15.9,
+    target: "$ 80,000",
+    delta: "13.2%",
+    deltaType: "moderateIncrease",
+  },
+  {
+    title: "Profit",
+    metric: "$ 45,564",
+    progress: 36.5,
+    target: "$ 125,000",
+    delta: "23.9%",
+    deltaType: "increase",
+  },
+  {
+    title: "Customers",
+    metric: "1,072",
+    progress: 53.6,
+    target: "2,000",
+    delta: "10.1%",
+    deltaType: "moderateDecrease",
+  },
+];
+
+export default function KpiCardGrid() {
+  return (
+    <Grid numItemsLg={3} className="mt-6 gap-6">
+      {kpiData.map((item) => (
+        <Card key={item.title}>
+          <Flex alignItems="start">
+            <div className="truncate">
+              <Text>{item.title}</Text>
+              <Metric className="truncate">{item.metric}</Metric>
+            </div>
+            <BadgeDelta deltaType={item.deltaType}>{item.delta}</BadgeDelta>
+          </Flex>
+          <Flex className="mt-4 space-x-2">
+            <Text className="truncate">{`${item.progress}% (${item.metric})`}</Text>
+            <Text>{item.target}</Text>
+          </Flex>
+          <ProgressBar value={item.progress} className="mt-2" />
+        </Card>
+      ))}
+    </Grid>
+  );
+}
