c0ae3b722c9f856e00d6dc625c1f24d93db445ff
Ricardo <ricardisimofuentes@hotmail.com>
2023-09-19 23:47:24 -0600

    Botones editar y eliminar funcionales en el apartado agregar productos

--- src/components/productos.jsx
+++ src/components/productos.jsx
@@ -1,131 +1,14 @@
-import { useEffect, useState } from "react";
 import AgregarProducto from "./productsUI/agregarProductos";
-
-import Swal from "sweetalert2";
 import MostrarProductos from "./productsUI/mostrarProductos";
 
-import "../css/productos.css";
-
 const Productos = (props) => {
-
-
-  const estadoInicialProducto = {
-    nombre: "",
-    descripcion: "",
-    existencias: "",
-    caducidad: "",
-    precio: "",
-  };
-
-  const [producto, setFormulario] = useState(estadoInicialProducto); // Initialize with the initial state
-
-  const [productos, setProductos] = useState([]);
-
-  const ManejarFormulario = ({ target: { name, value } }) => {
-    setFormulario({ ...producto, [name]: value });
-  };
-
-  // const handleAddProduct = (e) => {
-  //   e.preventDefault();
-  //   console.log("Handling add product");
-  //   console.log("Current producto state:", producto);
-
-  //   // Required fields check
-  //   if (
-  //     !producto.nombre ||
-  //     !producto.descripcion ||
-  //     !producto.existencias ||
-  //     !producto.caducidad ||
-  //     !producto.precio
-  //   ) {
-  //     Swal.fire({
-  //       title: "Error",
-  //       text: "Por favor, complete todos los campos.",
-  //       icon: "warning",
-  //       confirmButtonText: "OK",
-  //     });
-  //     return;
-  //   }
-
-  //   // Numeric fields validation
-  //   if (isNaN(Number(producto.existencias)) || isNaN(Number(producto.precio))) {
-  //     Swal.fire({
-  //       title: "Error",
-  //       text: "Los campos Existencias y Precio deben ser números válidos.",
-  //       icon: "error",
-  //       confirmButtonText: "OK",
-  //     });
-  //     return;
-  //   }
-
-  //   // Check for negative numbers
-  //   if (Number(producto.existencias) < 0 || Number(producto.precio) < 0) {
-  //     Swal.fire({
-  //       title: "Error",
-  //       text: "Los campos Existencias y Precio no pueden ser números negativos.",
-  //       icon: "error",
-  //       confirmButtonText: "OK",
-  //     });
-  //     return;
-  //   }
-
-  //   // If all validations pass, add the product and reset the form
-  //   setProductos([...productos, producto]);
-  //   setFormulario(estadoInicialProducto);
-
-  //   console.log("Producto state after reset:", producto);
-  // };
-
-  const handleDeleteProduct = (index) => {
-    const updatedProductos = [...productos];
-    updatedProductos.splice(index, 1);
-    setProductos(updatedProductos);
-  };
-
-  const [editMode, setEditMode] = useState(false);
-  const [editIndex, setEditIndex] = useState(-1);
-
-  const handleEdit = (index) => {
-    setEditMode(true);
-    setEditIndex(index);
-  };
-
-  const handleEditChange = (e, field) => {
-    const updatedProductos = [...productos];
-    updatedProductos[editIndex][field] = e.target.value;
-    setProductos(updatedProductos);
-  };
-
-  const handleSaveEdit = (index) => {
-    setEditMode(false);
-    setEditIndex(-1);
-    // You can save changes to your backend or update state as needed
-  };
-
-  // const createProduct = (productData) => {
-  //   return axios.post("/api/products", productData);
-  // };
-
   return (
     <div id="wrapper">
       <div id="content">
         <div className="container">
-          <AgregarProducto
-            producto={producto}
-            // onAddProduct={handleAddProduct}
-            onFormChange={ManejarFormulario}
-            
-          />
+          <AgregarProducto />
 
-          <MostrarProductos
-            productos={productos}
-            editMode={editMode}
-            editIndex={editIndex}
-            onEdit={handleEdit}
-            onEditChange={handleEditChange}
-            onSaveEdit={handleSaveEdit}
-            onDeleteProduct={handleDeleteProduct}
-          />
+          <MostrarProductos />
         </div>
       </div>
     </div>
--- src/components/productsUI/mostrarProductos.jsx
+++ src/components/productsUI/mostrarProductos.jsx
@@ -9,142 +9,191 @@ import {
 
 const MostrarProductos = (props) => {
   const [productos, setProductos] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editedProduct, setEditedProduct] = useState({});
 
   useEffect(() => {
     // Realiza una llamada a la API para obtener los productos desde el servidor
-    fetch('http://localhost:3000/productos')
+    fetch("http://localhost:3000/productos")
       .then((response) => response.json())
       .then((data) => {
         setProductos(data); // Actualiza el estado con los datos de la base de datos
       })
       .catch((error) => {
-        console.error('Error al obtener productos: ' + error);
+        console.error("Error al obtener productos: " + error);
       });
   }, []);
 
+  const onEditChange = (e, field) => {
+    const updatedProduct = { ...editedProduct };
+    updatedProduct[field] = e.target.value;
+    setEditedProduct(updatedProduct);
+  };
+
+  const onEdit = (index) => {
+    setEditMode(true);
+    setEditIndex(index);
+    setEditedProduct({ ...productos[index] });
+  };
+
+  const onSaveEdit = (index) => {
+    // Realizar una solicitud PUT para guardar los cambios en la base de datos
+    fetch(`http://localhost:3000/productos/${productos[index].id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editedProduct),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setEditMode(false);
+        setEditIndex(null);
+        // No necesitas restablecer editedProduct a un objeto vacío aquí
+        // Actualiza la lista de productos después de la edición
+        const updatedProducts = [...productos];
+        updatedProducts[index] = data;
+        setProductos(updatedProducts);
+      })
+      .catch((error) => {
+        console.error("Error al guardar la edición: " + error);
+      });
+  };
+
+  const onDeleteProduct = (index) => {
+    // Realizar una solicitud DELETE para eliminar el producto de la base de datos
+    fetch(`http://localhost:3000/productos/${productos[index].id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        // Elimina el producto de la lista después de la eliminación
+        const updatedProducts = [...productos];
+        updatedProducts.splice(index, 1);
+        setProductos(updatedProducts);
+      })
+      .catch((error) => {
+        console.error("Error al eliminar el producto: " + error);
+      });
+  };
+
   return (
-    <> 
-    <div className="container w-12 h-40 p-5 ml-10 mr-10 text-lg bg-white text-justify-center bg-opacity-300">
+    <>
+      <div className="container flex p-5 ml-10 mr-10 text-lg bg-white text-justify-center bg-opacity-300">
+        <div className="flex items-center justify-center mb-4 text-3xl font-bold text-black">
+          INVENTARIO
+        </div>
 
+        <table className="w-full border-collapse table-auto">
+          <thead>
+            <tr className="bg-[#3853DA] text-white">
+              <th className="py-2 text-lg">ID</th>
+              <th className="px-10 py-2 text-lg">NOMBRE</th>
+              <th className="px-8 py-2 text-lg">DESCRIPCION</th>
+              <th className="px-8 py-2 text-lg">EXISTENCIAS</th>
+              <th className="px-8 py-2 text-lg">CADUCIDAD</th>
+              <th className="px-10 py-2 text-lg">PRECIO</th>
+              <th className="py-2 text-lg">ACCIONES</th>
+            </tr>
+          </thead>
 
- <div className="flex items-center justify-center mb-4 text-3xl font-bold text-black">
-        INVENTARIO
-      </div>
-
-      <table className="w-full border-collapse table-auto">
-        <thead>
-          <tr className="bg-[#3853DA] text-white">
-            <th className="py-2 text-lg">ID</th>
-            <th className="px-10 py-2 text-lg">NOMBRE</th>
-            <th className="px-8 py-2 text-lg">DESCRIPCION</th>
-            <th className="px-8 py-2 text-lg">EXISTENCIAS</th>
-            <th className="px-8 py-2 text-lg">CADUCIDAD</th>
-            <th className="px-10 py-2 text-lg">PRECIO</th>
-            <th className="py-2 text-lg">ACCIONES</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {productos.map((product, index) => (
-            <tr className="text-black " key={index}>
-              <td className="px-4 py-2 border border-black">{index + 1}</td>
-              <td className="py-2 border border-black">
-                {props.editMode && props.editIndex === index ? (
-                  <input
-                    type="text"
-                    value={productos[index].nombre}
-                    onChange={(e) => props.onEditChange(e, "nombre")}
-                    className="py-2 text-sm"
-                  />
-                ) : (
-                  product.nombre
-                )}
-              </td>
-              <td className="py-2 border border-black">
-                {props.editMode && props.editIndex === index ? (
-                  <input
-                    type="text"
-                    value={productos[index].descripcion}
-                    onChange={(e) => props.onEditChange(e, "descripcion")}
-                    className="py-2 text-sm"
-                  />
-                ) : (
-                  product.descripcion
-                )}
-              </td>
-              <td className="py-2 border border-black">
-                {props.editMode && props.editIndex === index ? (
-                  <input
-                    type="number"
-                    value={productos[index].existencias}
-                    onChange={(e) => props.onEditChange(e, "existencias")}
-                    className="py-2 text-sm"
-                  />
-                ) : (
-                  product.existencias
-                )}
-              </td>
-              <td className="py-2 border border-black">
-                {props.editMode && props.editIndex === index ? (
-                  <input
-                    type="date"
-                    value={productos[index].caducidad}
-                    onChange={(e) => props.onEditChange(e, "caducidad")}
-                    className="py-2 text-sm"
-                  />
-                ) : (
-                  product.caducidad
-                )}
-              </td>
-              <td className="py-2 border border-black">
-                {props.editMode && props.editIndex === index ? (
-                  <input
-                    type="number"
-                    value={productos[index].precio}
-                    onChange={(e) => props.onEditChange(e, "precio")}
-                    className="py-2 text-sm"
-                  />
-                ) : (
-                  product.precio
-                )}
-              </td>
-
-              <td className="py-2 border border-black">
-                <td className="">
-                  {props.editMode && props.editIndex === index ? (
-                    <button
-                      className=" bg-[#FFD658] rounded-[10px] p-4 text-lg "
-                      onClick={() => props.onSaveEdit(index)}
-                    >
-                      <FontAwesomeIcon icon={faFileCircleCheck} size="2xl" />
-                    </button>
+          <tbody>
+            {productos.map((product, index) => (
+              <tr className="text-black " key={index}>
+                <td className="px-4 py-2 border border-black">{index + 1}</td>
+                <td className="py-2 border border-black">
+                  {editMode && editIndex === index ? (
+                    <input
+                      type="text"
+                      value={editedProduct.nombre}
+                      onChange={(e) => onEditChange(e, "nombre")}
+                      className="py-2 text-sm"
+                    />
                   ) : (
-                    <button
-                      className="bg-[#FFD658] rounded-[10px] p-4 text-lg "
-                      onClick={() => props.onEdit(index)}
-                    >
-                      <FontAwesomeIcon icon={faPenToSquare} size="2xl" />
-                    </button>
+                    product.nombre
                   )}
                 </td>
-                <td className="px-2 py-2 ">
-                  <button
-                    className="bg-[#FFD658] rounded-[10px] p-4 text-lg "
-                    onClick={() => props.onDeleteProduct(index)}
-                  >
-                    <FontAwesomeIcon icon={faTrashCan} size="2xl" />
-                  </button>
+                <td className="py-2 border border-black">
+                  {editMode && editIndex === index ? (
+                    <input
+                      type="text"
+                      value={editedProduct.descripcion}
+                      onChange={(e) => onEditChange(e, "descripcion")}
+                      className="py-2 text-sm"
+                    />
+                  ) : (
+                    product.descripcion
+                  )}
                 </td>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                <td className="py-2 border border-black">
+                  {editMode && editIndex === index ? (
+                    <input
+                      type="number"
+                      value={editedProduct.existencias}
+                      onChange={(e) => onEditChange(e, "existencias")}
+                      className="py-2 text-sm"
+                    />
+                  ) : (
+                    product.existencias
+                  )}
+                </td>
+                <td className="py-2 border border-black">
+                  {editMode && editIndex === index ? (
+                    <input
+                      type="date"
+                      value={editedProduct.caducidad}
+                      onChange={(e) => onEditChange(e, "caducidad")}
+                      className="py-2 text-sm"
+                    />
+                  ) : (
+                    product.caducidad
+                  )}
+                </td>
+                <td className="py-2 border border-black">
+                  {editMode && editIndex === index ? (
+                    <input
+                      type="number"
+                      value={editedProduct.precio}
+                      onChange={(e) => onEditChange(e, "precio")}
+                      className="py-2 text-sm"
+                    />
+                  ) : (
+                    product.precio
+                  )}
+                </td>
 
-
-      
-    </div>
-     
+                <td className="py-2 border border-black">
+                  <td className="">
+                    {editMode && editIndex === index ? (
+                      <button
+                        className=" bg-[#FFD658] rounded-[10px] p-4 text-lg "
+                        onClick={() => onSaveEdit(index)}
+                      >
+                        <FontAwesomeIcon icon={faFileCircleCheck} size="2xl" />
+                      </button>
+                    ) : (
+                      <button
+                        className="bg-[#FFD658] rounded-[10px] p-4 text-lg "
+                        onClick={() => onEdit(index)}
+                      >
+                        <FontAwesomeIcon icon={faPenToSquare} size="2xl" />
+                      </button>
+                    )}
+                  </td>
+                  <td className="px-2 py-2 ">
+                    <button
+                      className="bg-[#FFD658] rounded-[10px] p-4 text-lg "
+                      onClick={() => onDeleteProduct(index)}
+                    >
+                      <FontAwesomeIcon icon={faTrashCan} size="2xl" />
+                    </button>
+                  </td>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
