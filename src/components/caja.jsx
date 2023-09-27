111379ff41412ee41f210279ff0ca2f911588a86
FranciscoRauda <raudamejia08@outlook.com>
2023-09-26 19:22:35 -0600

    trabaje el buscador en apartado caja

--- src/components/caja.jsx
+++ src/components/caja.jsx
@@ -1,23 +1,26 @@
 import BarraBusqueda from "./searchBar/searchBar";
 
+
 const Caja = () => {
+
+  
   return (
     <div>
-      <div id="wrapper">
-        <div id="content">
+      <div className="bg-slate-200  h-[60rem] ">
+        <div id="">
           <div className="container">
-            <div className="container w-12 h-40 p-5 ml-10 mr-10 text-lg bg-white text-justify-center bg-opacity-300">
+            <div className="container w-40 h-100 p-5 ml-10 mr-10 text-lg bg-white text-justify-center bg-opacity-300">
               {" "}
               En esta parte, los comerciantes podrán procesar pagos de los
               clientes. Aquí es donde se registrarían los productos que el
               cliente desea comprar, se calcularía el monto total y se llevaría
               a cabo el proceso de pago.
             </div>
-
+             
             <BarraBusqueda />
-
-            <div className="">
-              <div className="mt-3 text-lg text-center">FACTURACION</div>
+          
+           <div className="">
+            <div className="mt-3 text-lg text-center">FACTURACION</div>
                        
               <table class="min-w-full">
                 <thead>
@@ -33,41 +36,21 @@ const Caja = () => {
                     </th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      6
-                    </td>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      CEPILLO
-                    </td>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      2.36
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      2
-                    </td>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      PASTA
-                    </td>
-                    <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
-                      2.00
-                    </td>
-                  </tr>
-                </tbody>
+                 
               </table>
-
-              <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded mt-3 ml-[30rem]">
-                Ingresar
-              </button>
+              
+              
             </div>
+            
           </div>
         </div>
+        <button class="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded mt-[34rem] ml-[30rem]">
+                Ingresar
+              </button>
       </div>
+      
     </div>
+    
   );
 };
 export default Caja;
--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,16 +1,56 @@
 
-
+import React, { useState, useEffect} from "react";
 const BarraBusqueda = () =>{
 
-    return(
-        <div className="bg-white bg-opacity-300 container h-20  rounded-xl center w-12 flex max-w-[74rem] mx- ">
+   // setear los hooks  usestate  
+   const[users,Setusers] = useState ([])
+   const[ search,setsearch] = useState ("")
+
+   // funcion para obtener datos de una api
+   const URL = 'https://jsonplaceholder.typicode.com/users'
+   
+   const showData = async () =>{
+  const  response = await  fetch(URL)
+  const   data = await response.json()
+   /*console.log(data)*/
+   Setusers(data)
+   }
+    // funcion de busqueda
+
+  useEffect( ()=>{
+    showData()
+  } , [])
+
+  // funcion buscador
+    const searcher = (e) =>{
+      setsearch(e.target.value)
+      console.log(e.target.value)
+    }
+
+    //metodo de filtrado 
+    let resultado = []
+    if(!search)
+    {
+        resultado = users
+    }else{
+         resultado = users.filter( (dato) =>
+         dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+     )
+    }
+  
+    return (
+
+       <div className="bg-white bg-opacity-300 container h-20  rounded-xl center w-12 flex max-w-[74rem] mx- ">
                   <div className="h-24 -mt-4 text-black-600 center">
-                    <input
+                    <input 
+                      value={search}
+                       onChange={searcher}
                       className="border-black rounded-full te39xt-sm w-[25rem] h-15 pr-14 bg-red border-5 px-200 focus:outline-none"
                       type="search"
                       name="search"
                       placeholder="Buscar"
-                    />
+                      />
+                     
                     <button
                       type="submit"
                       className="absolute top-0 right-0 mr-4 mt-7"
@@ -18,7 +58,34 @@ const BarraBusqueda = () =>{
                       
                     </button>
                   </div>
+
+                 <tbody > 
+                      { resultado.map( (user) =>(
+                                 <tr key={user.id} >
+                                 <td class="px-[5rem] py-4 whitespace-nowrap border-b border-gray-300">
+                                   {user.name} 
+                                  
+                                 </td>
+                                 <td class="px-[5rem] py-4 whitespace-nowrap border-b border-gray-300">
+                                 {user.username} 
+                                 </td>
+                                 <td class="px-[10rem] py-4 whitespace-nowrap border-b border-gray-300">
+                                 {user.username} 
+                                 </td>
+                               </tr> 
+                               
+                               )) }
+                     </tbody>
+    
+
+
                 </div>
+                      
+                
+
+
+
+
     )
 };
 
